b76c043399f3934271466400da3d565c7033d300
mmebsout <magalimebsout+dev@gmail.com>
2019-11-25 15:58:59 +0100

    Added Dockerfile for datacube only
    added Dockerfile for a docker image that runs only Datacube Node.js app

---
+++ docker_datacube/run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker run --name datacube \
+            -d \
+            -p 8000:4200 \
+            datacube
+echo "Please launch : http://localhost:8000/DataCube"
---
+++ docker_datacube_and_server/clean.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker rm datacube_and_server -f
