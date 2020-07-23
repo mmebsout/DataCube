7393837c2ae67e7f92768de7eaf0a2070b915fcf
mmebsout <magalimebsout+dev@gmail.com>
2020-07-23 11:55:30 +0200

    fixed comeback of app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,6 @@ import { LoginModule } from './login/login.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
-import { AboutModule } from './about/about.module';
 import { HttpCacheService } from './core';
 import { CachingInterceptor } from './core/http/http.cache.interceptor';
 
